69269c238e74c71fce798a325c8149b328cbd495
Zach Hamilton <zlhamilton90@gmail.com>
2020-10-05 00:06:53 -0400

    fin

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,15 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if start <= end:
+        middle_index = (start + end) // 2
+        if arr[middle_index] == target:
+            return middle_index
+        elif arr[middle_index] > target:
+            return binary_search(arr, target, start, middle_index)
+        elif arr[middle_index] < target:
+            return binary_search(arr, target, middle_index, end)
+    else:    
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -10,5 +19,31 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    def ascending_binary_search(arr, target, start, end):
+        if start <= end:
+            middle_index = (start + end) // 2
+            if arr[middle_index] == target:
+                return middle_index
+            elif arr[middle_index] > target:
+                return ascending_binary_search(arr, target, start, middle_index)
+            elif arr[middle_index] < target:
+                return ascending_binary_search(arr, target, middle_index, end)
+        else:    
+            return -1
 
+    def descending_binary_search(arr, target, start, end):
+        if start <= end:
+            middle_index = (start + end) // 2
+            if arr[middle_index] == target:
+                return middle_index
+            elif arr[middle_index] < target:
+                return descending_binary_search(arr, target, start, middle_index)
+            elif arr[middle_index] > target:
+                return descending_binary_search(arr, target, middle_index, end)
+        else:    
+            return -1
+    
+    if arr[0] < arr[-1]:        
+        return ascending_binary_search(arr, target, 0, len(arr)-1)
+    else:
+        return descending_binary_search(arr, target, 0, len(arr)-1)
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,20 +1,51 @@
+import random
+
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    a_index = 0
+    b_index = 0
+    merged_index = 0
+
+    # this will compare and add the smaller value in A nd B and then move to next of that list
+    while a_index < len(arrA) and b_index < len(arrB): # this while loop will end when one list is fully used
+        if arrA[a_index] < arrB[b_index]:
+            merged_arr[merged_index] = arrA[a_index]
+            a_index += 1
+        else:
+            merged_arr[merged_index] = arrB[b_index]
+            b_index += 1
+        merged_index += 1
+
+    # one of the two arrays will still have values so these while loops will add them in
+    while a_index < len(arrA):
+        merged_arr[merged_index] = arrA[a_index]
+        a_index += 1
+        merged_index += 1
+
+    while b_index < len(arrB):
+        merged_arr[merged_index] = arrB[b_index]
+        b_index += 1
+        merged_index += 1
 
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    if len(arr) > 1:
+        middle = len(arr) // 2
+        left = arr[:middle]
+        right = arr[middle:]
+        return merge(merge_sort(left), merge_sort(right))
+    else:
+        return arr
 
 
-    return arr
-
+    
+'''
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
@@ -25,4 +56,8 @@ def merge_in_place(arr, start, mid, end):
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+'''
+'''
+woo = random.sample(range(6000), 5000)
+print(merge_sort(woo))
+'''
